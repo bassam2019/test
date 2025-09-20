f068406caef3c10d030520c08caecd911ec6b188
bassam2019 <54856742+bassam2019@users.noreply.github.com>
2025-09-20 08:43:42 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,22 +2,13 @@
 <html lang="ar">
 <head>
     <meta charset="UTF-8">
-    <title>تسجيل الدخول</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Hello World - PHP</title>
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
-    <div class="login-container">
-        <h2>تسجيل الدخول</h2>
-        <form action="login.php" method="post">
-            <input type="text" name="username" placeholder="اسم المستخدم" required>
-            <input type="password" name="password" placeholder="كلمة المرور" required>
-            <input type="submit" value="دخول">
-        </form>
-        <?php if (!empty($error)) { ?>
-            <div class="error"><?php echo $error; ?></div>
-        <?php } elseif (!empty($message)) { ?>
-            <div class="message"><?php echo $message; ?></div>
-        <?php } ?>
+    <div class="container">
+        <h1><?php echo "مرحباً بالعالم!"; ?></h1>
+        <p></p>
     </div>
 </body>
 </html>
